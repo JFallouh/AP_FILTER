f252fd47b6ac13b68045a523818a09cd4f0f40ed
James <jamesfallouh@outlook.com>
2025-07-04 12:28:39 -0400

    after publishing

--- obj/Release/net9.0/win-x64/AP_filter.AssemblyInfo.cs
+++ obj/Release/net9.0/win-x64/AP_filter.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AP_filter")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5989a0bee5b3de5c227d9ca8a5eb3d855d2ea9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AP_filter")]
